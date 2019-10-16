36c68d34bc4a4e2111c452d71c80d6f8a0ffa621
isbnn <isb.cv@yandex.ru>
2019-10-16 22:50:44 +0300

    feat: completed task

--- src/queue.js
+++ src/queue.js
@@ -1,15 +1,32 @@
 const MaxHeap = require("./max-heap.js");
 
 class PriorityQueue {
-  constructor(maxSize) {}
+  constructor(maxSize = 30) {
+    this.maxSize = maxSize;
+    this.heap = new MaxHeap();
+  }
 
-  push(data, priority) {}
+  push(data, priority) {
+    if (this.size() === this.maxSize) {
+      throw new Error("Queue already has max size.");
+    }
+    this.heap.push(data, priority);
+  }
 
-  shift() {}
+  shift() {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty.");
+    }
+    return this.heap.pop();
+  }
 
-  size() {}
+  size() {
+    return this.heap.size();
+  }
 
-  isEmpty() {}
+  isEmpty() {
+    return this.heap.isEmpty();
+  }
 }
 
 module.exports = PriorityQueue;
